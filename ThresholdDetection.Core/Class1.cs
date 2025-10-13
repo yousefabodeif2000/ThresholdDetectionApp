ab20e1480f3191436b25c1885aaad65c9f09cb13
yousefabodeif2000 <83887183+yousefabodeif2000@users.noreply.github.com>
2025-10-13 13:49:24 +0300

    project setup

---
+++ ThresholdDetection.Core/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace ThresholdDetection.Core
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ ThresholdDetection.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace ThresholdDetection.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
